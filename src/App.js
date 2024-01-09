5cad5dfc927e8a01beb2fa4a17d9492990b068b7
azgar0053 <azgar53ali@gmail.com>
2024-01-09 11:58:28 +0530

    added new modal form

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import './App.css';
+import Modal from './Modal';
+
+function App() {
+  const [formData, setFormData]= useState({
+    username:'',
+    email:'',
+    phone:'',
+    dob:''
+  })
+  const openForm=()=>{
+    document.querySelector('.modal').style.display = 'block';
+    document.querySelector('.overlay').style.display ='block';
+    }
+
+    const closeForm=()=>{
+      document.querySelector('.modal').style.display = 'none';
+    document.querySelector('.overlay').style.display ='none';
+    }
+
+    const handleSubmit=(e)=>{
+      e.preventDefault();
+      const currentDate= new Date()
+      const enteredDate = new Date(formData.dob);
+      const isFutureDate =
+        enteredDate.getFullYear() > currentDate.getFullYear() ||
+        (enteredDate.getFullYear() === currentDate.getFullYear() &&
+          enteredDate.getMonth() > currentDate.getMonth()) ||
+        (enteredDate.getFullYear() === currentDate.getFullYear() &&
+          enteredDate.getMonth() === currentDate.getMonth() &&
+          enteredDate.getDate() > currentDate.getDate());
+      
+      if(formData.phone.length!==10){
+        console.log(formData.phone.length)
+        alert('Invalid phone number. Please enter a 10-digit phone number.')
+      }
+      if(isFutureDate){
+        alert('Invalid date of birth. Date of Birth cannot be in future')
+      }
+    }
+
+    const formatDate=(inputDate)=>{
+      const date = new Date(inputDate);
+      const day = date.getDate();
+      const month = date.getMonth()+1;
+      const year = date.getFullYear();
+      return `${day}-${month}-${year}`
+    }
+
+    const handleChange=(e)=>{
+      const {name , value}= e.target;
+
+      const updatedValue = name === 'dob' ? formatDate(value) : value;
+      setFormData((prev)=> ({...prev, [name]: updatedValue}))
+        }
+
+
+  return (
+    <div className="App">
+      <div className='overlay' onClick={closeForm}></div>
+      <h1>User Details Modal</h1>
+      <button onClick={openForm}>Open Form</button>
+      <Modal handleSubmit={handleSubmit} handleChange={handleChange}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Modal.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Modal = (props) => {
+
+const handleSubmit=(e)=>{
+    props.handleSubmit(e)
+}
+
+
+
+  return (
+    <div className="modal" style={{display:'none'}}>
+        <div className="modal-content">
+            <h1>Fill Details</h1>
+            <form onSubmit={handleSubmit}>
+            <label htmlFor='username'>Username:</label>
+            <input id='username' type='text' required value={props.username} onChange={props.handleChange} name='username'/><br/>
+            <label htmlFor='email'>Email Address:</label>
+            <input id='email' type='email' pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" required value={props.email} onChange={props.handleChange} name='email'/><br/>
+            <label htmlFor='phone'>Phone Number:</label>
+            <input id='phone' type='tel' required value={props.phone} onChange={props.handleChange} name='phone'/><br/>
+            <label htmlFor='dob'>Date of Birth:</label>
+            <input id='dob' type='date' required value={props.dob} onChange={props.handleChange} name='dob'/><br/>
+            <button className='submit-button'>Submit</button>
+            </form>
+        </div>
+</div>
+  )
+}
+
+export default Modal
