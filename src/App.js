72c99c2a40b293c2ee160b6afc1eaa50839a29b9
azgar0053 <azgar53ali@gmail.com>
2024-01-10 00:23:10 +0530

    removed prevent default

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     }
 
     const handleSubmit=(e)=>{
-      e.preventDefault();
+      // e.preventDefault();
       const currentDate= new Date()
       const enteredDate = new Date(formData.dob);
       const isFutureDate =
